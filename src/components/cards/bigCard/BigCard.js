2d0795acf12153577c2b5e299e47ba19a8d43835
Júlio Barreto <julio.costa@ccc.ufcg.edu.br>
2018-12-09 23:42:19 -0300

    ainda falta corrigir responsividade

--- src/components/cards/bigCard/BigCard.js
+++ src/components/cards/bigCard/BigCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const styles = bgColor => ({
     background: {
         backgroundColor: bgColor || 'white',
-        height: '100vh',
+        minHeight: '100vh',
     },
 });
 
